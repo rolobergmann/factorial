e7385be1b470462f33a369a0e9c4f5726878f42c
Alexander Beck V <alienxander.github@gmail.com>
2024-01-31 19:53:30 -0300

    Explicación de arrays

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,11 +1,46 @@
 let showResult = document.querySelector(".resultado");
+/*
+			&&: and o y lógico --> Para Todo sea true todas las condiciones deben ser true
+			||: or u o lógico --> Para Todo sea true al menos una condición debe ser true
+			!: not o negación --> Niega la condición, si condi. es true, resultado es false y viceversa
 
+			&&:
+
+			false && false = false
+			false && true = false
+			true && false = false
+			true && true = true
+
+			||:
+
+			false || false = false
+			false || true = true
+			true || false = true
+			true || true = true
+
+			!
+
+			true = false
+			false = true
+
+			var num = 3;
+
+			if(!(num == 3)){
+				console.log("OK");
+			}
+
+			if(num != 3){
+				console.log("OK");
+			}
+		*/
 $(document).ready(function () {
 	$("#numeroForm").submit(function (event) {
 		event.preventDefault(); // Evitar la recarga de la página
 		var numero = $("#numeroInput").val(); // Obtener el valor del input
-		numero = Number(numero); // Convertir a número
-
+		//numero = t
+		numero = Number(numero); // Convertir a número 
+		
+		console.log("Mi número!!!!!!: " + numero);
 		if (!isNaN(numero) && numero >= 1 && numero <= 20) {
 			var factorial = calcularFactorial(numero);
 			let result = document.createElement("div");
